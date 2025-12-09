d4c53c8399535bbe84a8a84a37c7e600e3f64a4c
Jonas Perez <00jonasperez@gmail.com>
2025-12-09 18:58:01 +0800

    chore: add MUI CssBaseline

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <CssBaseline />
       <body>
         <Main />
         <NextScript />
